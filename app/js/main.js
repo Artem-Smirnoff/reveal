9f2fdbbb8ab9b7688c04482d501109c08c957485
Artem Smirnov <parov91@mail.ru>
2023-11-27 19:47:42 +0300

    added header, hero, collaborative

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
 $(function () {
-
+  $('.header__burger').click(function () {
+    $('.header__burger').toggleClass('header__burger--close');
+    $('.header__list').toggleClass('header__list--active');
+  }) 
 })
